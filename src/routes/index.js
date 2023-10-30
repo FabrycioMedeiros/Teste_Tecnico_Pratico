c9b39342d638a642186ca7067b9c2e54120ee6e9
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-10-30 14:06:03 -0300

    Adição do import aluguel

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import automovel from "./automovelRoutes.js";
-import motorista from "./motoristaRoutes.js"
+import motorista from "./motoristaRoutes.js";
+import aluguel from "./aluguelRoutes.js";
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("Teste Técnico Prático - WebAPI"));
 
-    app.use(express.json(), automovel, motorista);
+    app.use(express.json(), automovel, motorista, aluguel);
 };
 
 export default routes;

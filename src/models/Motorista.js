04de8ffd8602d31d909665c9da599572a12b6cd3
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-11-01 17:57:01 -0300

    Correção Model Motorista.js

--- src/models/Motorista.js
+++ src/models/Motorista.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const motoristaSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    nome: { type: String, requerid: true }
+    nome: { type: String, required: true }
 }, { versionKey: false });
 
 const motorista = mongoose.model("motoristas", motoristaSchema);

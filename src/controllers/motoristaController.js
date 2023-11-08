ab5503b24258b1e4cb380e8b84e73fe0e93f1d54
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-11-08 14:55:18 -0300

    Correções em motoristaController.js para criação do MotoristaService.js

--- src/controllers/motoristaController.js
+++ src/controllers/motoristaController.js
@@ -1,52 +1,65 @@
 import { motorista } from "../models/Motorista.js";
+import MotoristaService from "../services/MotoristaService.js";
 
 class MotoristaController {
+  static async cadastrarMotorista(req, res) {
+    try {
+      const motoristaData = req.body;
+      const motoristaCriado = await MotoristaService.cadastrarMotorista(motoristaData);
+      res.status(201).json({ message: "Cadastro do motorista efetuado com sucesso", Motorista: motoristaCriado });
+    } catch (erro) {
+      res.status(400).json({ message: `${erro.message}` });
+    }
+  }
 
-    static async cadastrarMotorista (req, res) {
-      try {
-        const novoMotorista = await motorista.create(req.body);
-        res.status(201).json({ message: "Cadastro de motorista com sucesso", Motorista: novoMotorista });
-      } catch (erro) { res.status(500).json({ message: `${erro.message} - Falha ao cadastrar motorista` }); }
-    };
+  static async atualizarMotorista(req, res) {
+    try {
+      const id = req.params.id;
+      const motoristaData = req.body;
+      const motoristaAtualizado = await MotoristaService.atualizarMotorista(id, motoristaData);
+      res.status(200).json({ message: "Motorista atualizado com sucesso", Motorista: motoristaAtualizado });
+    } catch (erro) {
+      res.status(400).json({ message: `${erro.message}` });
+    }
+  }
 
-    static async atualizarMotorista (req, res) {
-      try {
-        const id = req.params.id;
-        await motorista.findByIdAndUpdate(id, req.body);
-        res.status(200).json({ message: "Motorista atualizado com sucesso"});
-      } catch (erro) { res.status(500).json({ message: `${erro.message} - Falha na atualização do motorista` }); }
-    };
+  static async excluirMotorista(req, res) {
+    try {
+      const id = req.params.id;
+      await MotoristaService.excluirMotorista(id);
+      res.status(200).json({ message: "Motorista excluído com sucesso" });
+    } catch (erro) {
+      res.status(400).json({ message: `${erro.message}` });
+    }
+  }
 
-    static async excluirMotorista (req, res) {
-      try {
-        const id = req.params.id;
-        await motorista.findByIdAndDelete(id, req.body);
-        res.status(200).json({ message: "Motorista excluido com sucesso"});
-      } catch (erro) { res.status(500).json({ message: `${erro.message} - Falha na exclusão do motorista` }); }
-    };
+  static async listarMotoristas(req, res) {
+    try {
+      const listaMotorista = await MotoristaService.listarMotoristas();
+      res.status(200).json(listaMotorista);
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message}` });
+    }
+  }
 
-    static async listarMotoristaPorId (req, res) {
-      try {
-        const id = req.params.id;
-        const motoristaEncontrado = await motorista.findById(id);
-        res.status(200).json(motoristaEncontrado);
-      } catch (erro) { res.status(500).json({ message: `${erro.message} - Falha na busca do motorista por id` }); }
-    };
+  static async listarMotoristaPorId(req, res) {
+    try {
+      const motoristaEncontrado = await MotoristaService.listarMotoristaPorId(req.params.id);
+      res.status(200).json(motoristaEncontrado);
+    } catch (erro) {
+      res.status(400).json({ message: `${erro.message}` });
+    }
+  }
 
-    static async listarMotoristaPorNome (req, res) {
-      const nome = req.query.nome;
-      try {
-        const motoristaPorNome = await motorista.find({ nome: nome });
-        res.status(200).json(motoristaPorNome);
-      } catch (erro) { res.status(500).json({ message: `${erro.message} - Falha na busca do motorista por nome` }); }
-    };
+  static async listarMotoristaPorNome(req, res) {
+    try {
+      const motoristaPorNome = await MotoristaService.listarMotoristaPorNome(req.query.nome);
+      res.status(200).json(motoristaPorNome);
+    } catch (erro) {
+      res.status(400).json({ message: `${erro.message}` });
+    }
+  }
 
-    static async listarMotoristas (req, res) {
-      try {
-        const listaMotorista = await motorista.find({});
-        res.status(200).json(listaMotorista);
-      } catch (erro) { res.status(500).json({ message: `${erro.message} - Falha na busca do motoristas` }); }
-    };
-};
+}
 
 export default MotoristaController;
